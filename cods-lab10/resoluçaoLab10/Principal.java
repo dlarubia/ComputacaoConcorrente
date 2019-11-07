87b4923da185775069d6fe54d3e40bc141a9628e
dlarubia <dlarubia@dcc.ufrj.br>
2019-11-07 13:19:22 -0300

    Programa funcionando

---
+++ cods-lab10/resoluçaoLab10/Principal.java
@@ -0,0 +1,77 @@
+import java.io.*; 
+import java.util.*; 
+
+class Vetor {
+    int quantidadeElementos;
+    int vetor[];
+
+    Vetor(int quantidadeElementos) {
+        this.quantidadeElementos = quantidadeElementos;
+        this.vetor = new int[quantidadeElementos];
+        for(int i = 0; i < quantidadeElementos; i++) {
+            this.vetor[i] = i;
+        }
+    }
+
+    void alteraElemento(int n, int posicao) {
+        this.vetor[posicao] = n;
+    } 
+
+    void imprimeVetor() {
+        System.out.println(Arrays.toString(this.vetor));
+    }
+}
+
+class SomaVetor implements Runnable {
+
+    public Vetor vetorA;
+    public Vetor vetorB;
+    public Vetor vetorC;
+    public int tid, nthreads;
+  
+    SomaVetor(Vetor vetorA, Vetor vetorB, Vetor vetorC, int nthreads, int tid) {
+        this.tid = tid;
+        this.nthreads = nthreads;
+        this.vetorA = vetorA;
+        this.vetorB = vetorB;
+        this.vetorC = vetorC;
+    }
+    
+    public void run() {
+        for (int i = tid; i < this.vetorA.vetor.length; i += this.nthreads) {
+            this.vetorC.vetor[i] = this.vetorA.vetor[i] + this.vetorB.vetor[i];
+        }
+    }
+}
+
+class Principal {
+    static final int N = 10;
+
+    public static void main (String[] args) {
+        Thread[] threads = new Thread[N];
+        Vetor vetorA = new Vetor(20);
+        Vetor vetorB = new Vetor(20);
+        Vetor vetorC = new Vetor(20);
+
+        for(int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new SomaVetor(vetorA, vetorB, vetorC, N, i));
+        }
+
+        for(int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+
+        for(int i = 0; i < threads.length; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+
+        vetorA.imprimeVetor();
+        vetorB.imprimeVetor();
+        vetorC.imprimeVetor();
+
+    }
+}
